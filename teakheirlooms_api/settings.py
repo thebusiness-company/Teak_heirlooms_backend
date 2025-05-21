de769436c8110555d1da56d32230cbd95956d212
thebusiness-company <thebusinesscomponyo12@gmail.com>
2025-05-21 15:08:49 +0530

    database conf

--- teakheirlooms_api/settings.py
+++ teakheirlooms_api/settings.py
@@ -89,9 +89,13 @@ WSGI_APPLICATION = 'teakheirlooms_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("NAME"),
+        "USER":  os.getenv("USER"),
+        "PASSWORD":  os.getenv("PASSWORD"),
+        "HOST":  os.getenv("HOST"),
+        "PORT":  os.getenv("PORT"),
     }
 }
 
